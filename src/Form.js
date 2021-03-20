3176c51ef905a0cef21b22fc63da8df82963b7fe
peashooter45 <balvinpatrick@yahoo.com>
2021-03-21 00:11:13 +0400

    Updated
    If todo list is blank it will not be save, it will alert instead

--- src/Form.js
+++ src/Form.js
@@ -3,21 +3,28 @@ import React from "react";
 const Form = ({ inputText, setInputText, setTodos, todos }) => {
   const inputTextHandler = e => {
     setInputText(e.target.value);
+    console.log(e.target.value);
   };
 
   const submitTodoHandler = e => {
     e.preventDefault();
-    setTodos([
-      ...todos,
-      { text: inputText, completed: false, id: Math.random() * 1000 },
-    ]);
-    setInputText("");
+    if (e.target.parentElement.previousSibling.defaultValue === "") {
+      alert("Todo list is empty!");
+    } else {
+      setTodos([
+        ...todos,
+        { text: inputText, completed: false, id: Math.random() * 1000 },
+      ]);
+      setInputText("");
+    }
+    //console.log(e.target.parentElement.previousSibling.defaultValue);
   };
 
   return (
     <form className="form">
       <input
         type="text"
+        required
         className="inputText"
         onChange={inputTextHandler}
         value={inputText}
